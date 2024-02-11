c5ebbe1509918bcf2fc142a699d1eff228064841
CloudyKrypton <janetfu04@gmail.com>
2024-02-11 02:05:22 -0500

    Add projects section, fix tab bar

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const linkedin = document.getElementById("linkedin");
 const darkButton = document.getElementById("dark-button");
 const buttonImage = document.getElementById("button-image");
-const github = document.getElementById("github");
+const githubs = document.getElementsByClassName("github");
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
@@ -12,6 +12,8 @@ function myFunction() {
     element2.classList.toggle("dark-about-me-text");
     var element3 = document.querySelector(".tabs");
     element3.classList.toggle("dark-tabs");
+    var element4 = document.querySelector(".credit");
+    element4.classList.toggle("dark-credit");
     const tablinks = document.querySelectorAll('.tablink');
     tablinks[0].classList.toggle("dark-tablink");
     tablinks[1].classList.toggle("dark-tablink");
@@ -25,6 +27,9 @@ function myFunction() {
     const positionTitles = document.querySelectorAll('.position-title');
     positionTitles[0].classList.toggle("dark-position-title");
     positionTitles[1].classList.toggle("dark-position-title");
+    const projectCards = document.querySelectorAll('.project-card');
+    projectCards[0].classList.toggle("dark-project-card");
+    projectCards[1].classList.toggle("dark-project-card");
 }
 
 darkButton.addEventListener("click", function() {
@@ -50,14 +55,33 @@ darkButton.addEventListener("click", function() {
         linkedin.onmouseover = function() {lOver()};
         linkedin.onmouseout = function() {lOut()};
     }
-    if (github.src.endsWith("images/github.png") || github.src.endsWith("images/github-hover.png")) {
-        github.src = "images/github-dark.png";
-        github.onmouseover = function() {gdOver()};
-        github.onmouseout = function() {gdOut()};
-    } else {
-        github.src = "images/github.png";
-        github.onmouseover = function() {gOver()};
-        github.onmouseout = function() {gOut()};
+    for (var i = 0; i < githubs.length; i++) {
+        if (githubs[i].src.endsWith("images/github.png") || 
+        githubs[i].src.endsWith("images/github-hover.png")) {
+            githubs[i].src = "images/github-dark.png";
+            if (i == 0) {
+                githubs[0].onmouseover = function() {gdOver0()};
+                githubs[0].onmouseout = function() {gdOut0()};
+            } else if (i == 1) {
+                githubs[1].onmouseover = function() {gdOver1()};
+                githubs[1].onmouseout = function() {gdOut1()};
+            } else if (i == 2) {
+                githubs[2].onmouseover = function() {gdOver2()};
+                githubs[2].onmouseout = function() {gdOut2()};
+            }
+        } else {
+            githubs[i].src = "images/github.png";
+            if (i == 0) {
+                githubs[0].onmouseover = function() {gOver0()};
+                githubs[0].onmouseout = function() {gOut0()};
+            } else if (i == 1) {
+                githubs[1].onmouseover = function() {gOver1()};
+                githubs[1].onmouseout = function() {gOut1()};
+            } else if (i == 2) {
+                githubs[2].onmouseover = function() {gOver2()};
+                githubs[2].onmouseout = function() {gOut2()};
+            }
+        }
     }
 });
 function ldOver() {
@@ -72,15 +96,39 @@ function lOver() {
 function lOut() {
     linkedin.src = "images/linkedin.png";
 }
-function gdOver() {
-    github.src = "images/github-hover-dark.png";
+function gdOver0() {
+    githubs[0].src = "images/github-hover-dark.png";
 }
-function gdOut() {
-    github.src = "images/github-dark.png";
+function gdOut0() {
+    githubs[0].src = "images/github-dark.png";
 }
-function gOver() {
-    github.src = "images/github-hover.png";
+function gOver0() {
+    githubs[0].src = "images/github-hover.png";
 }
-function gOut() {
-    github.src = "images/github.png";
+function gOut0() {
+    githubs[0].src = "images/github.png";
+}
+function gdOver1() {
+    githubs[1].src = "images/github-hover-dark.png";
+}
+function gdOut1() {
+    githubs[1].src = "images/github-dark.png";
+}
+function gOver1() {
+    githubs[1].src = "images/github-hover.png";
+}
+function gOut1() {
+    githubs[1].src = "images/github.png";
+}
+function gdOver2() {
+    githubs[2].src = "images/github-hover-dark.png";
+}
+function gdOut2() {
+    githubs[2].src = "images/github-dark.png";
+}
+function gOver2() {
+    githubs[2].src = "images/github-hover.png";
+}
+function gOut2() {
+    githubs[2].src = "images/github.png";
 }
